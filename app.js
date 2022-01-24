9377867dbfcc05a4f55b0b411792c8159c38204c
I_am_jam <14906997+iamjam2@users.noreply.github.com>
2022-01-24 21:10:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function palindrome(str) {
   let backward = ""; //To store the reversed string.
-  let regEx = /[^a-z0-9]/g; //Regular expression fo identify letters and numbers in the string.
+  let regEx = /[^a-z0-9]/g; //Regular expression fo identify non alphanumeric characters in the string.
   
   str = str.toLowerCase().trim().replace(regEx, ""); //String functions to make all characters lowercase, trim the string & remove non-alphanumeric characters. 
   
